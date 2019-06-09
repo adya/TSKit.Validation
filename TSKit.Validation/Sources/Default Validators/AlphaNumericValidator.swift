0dc6aecea5a9c9f6f296e18cb79f09ce5987ef2a
Arkadii Hlushchevskyi <adya.glu@gmail.com>
2019-06-09 14:54:47 +0300

    Added Alphanumeric validator.

---
+++ TSKit.Validation/Sources/Default Validators/AlphaNumericValidator.swift
@@ -0,0 +1,24 @@
+/// - Since: 01/20/2018
+/// - Author: Arkadii Hlushchevskyi
+/// - Copyright: © 2018. Arkadii Hlushchevskyi.
+/// - Seealso: https://github.com/adya/TSKit.Validation/blob/master/LICENSE.md
+
+import Foundation
+
+/// Validates alphanumeric characters only.
+public class AlphaNumericValidator: PatternValidator {
+
+    public enum ValidationError: String, Error {
+
+        case mismatchNumbers = "String contains non alpha-numeric characters"
+
+        public var description: String {
+            return rawValue
+        }
+    }
+
+    public init() {
+        super.init(pattern: "\\w+",
+                   error: ValidationError.mismatchNumbers)
+    }
+}
